230590eece78ee4c17313188e9c7064a0eb93507
setLore <setlore@proton.me>
2023-08-11 02:05:23 +0300

    Added timer and more error handling.

--- usernameGenerator.py
+++ usernameGenerator.py
@@ -1,14 +1,19 @@
 import random
+import time
+
 from RandomWord import random_word
 numOfUsernames=0
-while numOfUsernames==0:
+while numOfUsernames <= 0:
     try:
         numOfUsernames = int(input("How many usernames do you want to generate? "))
+        if numOfUsernames <= 0:
+            print("Please enter a number higher than 0.")
     except ValueError:
         print("You need to enter a number.")
+
     
 
-
+start = time.time()
 adjective = random_word('english-adjectives.txt',1000)
 noun = random_word('english-nouns.txt',1000)
 for usernames in range(numOfUsernames):
@@ -19,3 +24,5 @@ for usernames in range(numOfUsernames):
 
     file = open('usernames.txt','a')
     file.write(f'{username}\n')
+end = time.time()
+print(f"\n You generated {numOfUsernames} usernames in {round((end-start),5)} seconds")
